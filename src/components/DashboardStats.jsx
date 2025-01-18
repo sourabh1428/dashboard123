dd6611d2279fc186c41eaafe00b4dfdc3f49c671
Sourabh <sppathak1428@gmail.com>
2025-01-19 04:16:02 +0530

    sa

--- src/components/DashboardStats.jsx
+++ src/components/DashboardStats.jsx
@@ -4,7 +4,8 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 import { LineChart, BarChart } from './Charts';
-import WhatsAppNotification from './WhatsappNotification.jsx';
+import WhatsAppNotification from './WhatsappNotification';
+
 
 const DashboardStats = () => {
   const lineChartData = [
